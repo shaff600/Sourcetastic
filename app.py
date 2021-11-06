5e49dd1292c1fefabd4b9364b5f26fb2b62d5ffe
shaff600 <shafyl@live.co.uk>
2021-11-06 18:34:49 +0000

    Add resources structure

--- app.py
+++ app.py
@@ -88,6 +88,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_resources")
+def add_resources():
+    return render_template("add_resources.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
